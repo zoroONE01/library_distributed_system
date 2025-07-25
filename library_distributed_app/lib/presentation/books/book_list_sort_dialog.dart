741a4953ebf06289cc9a0e3399dfac8c2d817c79
TuNT <tunt2@runsystem.net>
2025-07-25 16:45:18 +0700

    feat: Add API endpoints for coordinator and site services, and define sorting enums

--- library_distributed_app/lib/presentation/books/book_list_sort_dialog.dart
+++ library_distributed_app/lib/presentation/books/book_list_sort_dialog.dart
@@ -1,7 +1,7 @@
 // ignore: implementation_imports
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
-import 'package:library_distributed_app/core/constants/app_enum.dart';
+import 'package:library_distributed_app/core/constants/enums.dart';
 import 'package:library_distributed_app/core/extensions/theme_extension.dart';
 
 class BookListSortDialog extends HookWidget {
